4517c074e6e52ffca6229347aa31eecfad4b9ab4
flavinus <>
2022-12-16 16:33:17 +0100

    first step using go channels

--- nasty.go
+++ nasty.go
@@ -10,6 +10,13 @@ import (
 	"strings"
 )
 
+// SAMPLE
+
+type Sample struct {
+	c int
+	a int
+}
+
 // WAVE FORM
 
 func NewWaveform(filename string) *Waveform {
@@ -24,8 +31,6 @@ func NewWaveform(filename string) *Waveform {
 	w := Waveform{}
 	json.Unmarshal(byteValue, &w)
 
-	w.loadStates()
-
 	return &w
 }
 
@@ -33,33 +38,115 @@ type Waveform struct {
 	Description string `json:"description"`
 	ChanClient  []int  `json:"channel0"`
 	ChanAgent   []int  `json:"channel1"`
-
-	samples int
-
-	stateClient []int
-	stateAgent  []int
-	discussion  string
 }
 
-func (w *Waveform) loadStates() {
+func (w *Waveform) ToChannel(output chan Sample) {
+	samples := int(math.Min(float64(len(w.ChanClient)), float64(len(w.ChanAgent))))
+	go func() {
+		for i := 0; i < samples; i++ {
+			output <- Sample{c: w.ChanClient[i], a: w.ChanAgent[i]}
+		}
+		output <- Sample{c: -1, a: -1} // really usefull ? i'm not sure it still works...
 
-	w.stateClient = []int{}
-	w.stateAgent = []int{}
+		close(output)
+	}()
+}
 
-	w.samples = int(math.Min(float64(len(w.ChanClient)), float64(len(w.ChanAgent))))
+// ANALYSER
 
-	for i := 0; i < w.samples; i++ {
-		sc := w.getChanStatus(w.ChanClient[i])
-		w.stateClient = append(w.stateClient, sc)
+type Analyser struct {
+	score      int
+	samples    int
+	discussion string
+	debug      bool
+}
 
-		sa := w.getChanStatus(w.ChanAgent[i])
-		w.stateAgent = append(w.stateAgent, sa)
+func (a *Analyser) raiseScore(amount int, message string) {
+	a.score = a.score + amount
+	fmt.Printf("\n - Raise score: +%d : %s", amount, message)
+}
 
-		w.discussion = w.discussion + w.getDiscussionStatus(sc, sa)
+func (a *Analyser) run(input chan Sample) bool {
+
+	a.score = 0
+	a.samples = 0
+	a.discussion = ""
+
+	psc, psa := -1, -1 // previous state
+	dsc, dsa := 0, 0   // beginning of state
+
+	for sample := range input {
+		//fmt.Printf("\n<< receive : %+v ", sample)
+		sc := getChanStatus(sample.c)
+		sa := getChanStatus(sample.a)
+		d := getDiscussionStatus(sc, sa)
+
+		psc, dsc = a.watchStatus("Client", a.samples, sc, psc, dsc)
+		psa, dsa = a.watchStatus("Agent", a.samples, sa, psa, dsa)
+		a.discussion = a.discussion + d
+
+		a.samples = a.samples + 1
 	}
+
+	return a.results()
+
 }
 
-func (w *Waveform) getChanStatus(volume int) int {
+func (a *Analyser) watchStatus(who string, i, sc, psc, dsc int) (int, int) {
+
+	if sc != psc { // state changed
+		count := i - dsc
+		if psc == 0 { // mute ended
+			a.raiseScore(count*5, fmt.Sprintf("%s remained muted during %.2fs", who, float64(count)*0.1))
+
+			// TODO: BUG: this event is not raised anymore
+			if sc == -1 && dsc < 30 { // muted since more than 3s at end of call
+				a.raiseScore(1000, fmt.Sprintf("%s was muted at the end of conversation", who))
+			}
+		}
+
+		psc = sc
+		dsc = i
+	}
+	return psc, dsc
+}
+
+func (a *Analyser) results() bool {
+
+	// Eval repartitions ( int % )
+	pnobody := float64(strings.Count(a.discussion, "_")) / float64(a.samples) * 100.0
+	pclient := float64(strings.Count(a.discussion, "c")) / float64(a.samples) * 100.0
+	pagent := float64(strings.Count(a.discussion, "a")) / float64(a.samples) * 100.0
+	pboth := float64(strings.Count(a.discussion, "#")) / float64(a.samples) * 100.0
+
+	// Update score according to repartions stats
+
+	// Si il y a eu beaucoup de collisions
+	if pboth > 20 {
+		a.raiseScore(int(50*pboth), "many collisions during discussion")
+	}
+
+	// Si l'agent à très peu parlé
+	if pagent < 10 {
+		a.raiseScore(1000, "agent didn't speak very much")
+	}
+
+	// Eval overall result and display
+	ratio := float64(a.score) / float64(a.samples)
+
+	if a.debug {
+		fmt.Printf("\nDISCUSSION: %s", a.discussion)
+	}
+	fmt.Printf("\nREPARTITION: silent=%.2f%%  client=%.2f%%  agent=%.2f%%  both=%.2f%%", pnobody, pclient, pagent, pboth)
+	fmt.Printf("\nRESULT     : samples=%d  score=%d  => [ RATIO = %.2f ]", a.samples, a.score, ratio)
+	fmt.Printf("\n>>\n")
+
+	return ratio < 2
+}
+
+// OTHER FUNCS
+
+func getChanStatus(volume int) int {
 	switch {
 	case volume == 0:
 		return 0
@@ -70,7 +157,7 @@ func (w *Waveform) getChanStatus(volume int) int {
 	}
 }
 
-func (w *Waveform) getDiscussionStatus(sc, sa int) string {
+func getDiscussionStatus(sc, sa int) string {
 	if sc < 2 && sa < 2 {
 		return "_"
 	} else if sc > 1 && sa > 1 {
@@ -83,101 +170,24 @@ func (w *Waveform) getDiscussionStatus(sc, sa int) string {
 	return ""
 }
 
-// ANALYSER
+func analyseFile(filename string) bool {
 
-type Analyser struct {
-	score int
-	debug bool
-}
+	w := NewWaveform(filename)
 
-func (a *Analyser) raiseScore(amount int, message string) {
-	a.score = a.score + amount
-	fmt.Printf("\n - Raise score: +%d : %s", amount, message)
-}
+	schan := make(chan Sample)
+	w.ToChannel(schan)
 
-func (a *Analyser) run(w *Waveform) {
-
-	fmt.Printf("\n<< ANALYSE : %s ", w.Description)
-	if a.debug {
-		fmt.Printf("\n Client : states=%v", w.stateClient)
-		fmt.Printf("\n Agent  : states=%v", w.stateAgent)
-	}
-
-	a.score = 0
-
-	psc, psa := -1, -1 // previous state
-	dsc, dsa := 0, 0   // beginning of state
-
-	// Watch client and agent states changes
-	for i := 0; i < w.samples; i++ {
-		psc, dsc = a.watchStatus("Client", i, w.stateClient[i], psc, dsc)
-		psa, dsa = a.watchStatus("Agent", i, w.stateAgent[i], psa, dsa)
-	}
-
-	// We watch status once again at the end
-	a.watchStatus("Client", w.samples, -1, psc, dsc)
-	a.watchStatus("Agent", w.samples, -1, psa, dsa)
-
-	// Results
-	a.results(w)
-}
-
-func (a *Analyser) watchStatus(who string, i, sc, psc, dsc int) (int, int) {
-
-	if sc != psc { // state changed
-		count := i - dsc
-		if psc == 0 { // mute ended
-			a.raiseScore(count*5, fmt.Sprintf("%s remained muted during %.2fs", who, float64(count)*0.1))
-			if sc == -1 && dsc < 30 { // muted since more than 3s at end of call
-				a.raiseScore(1000, fmt.Sprintf("%s was muted at the end of conversation", who))
-			}
-		}
-
-		psc = sc
-		dsc = i
-	}
-	return psc, dsc
-}
-
-func (a *Analyser) results(w *Waveform) {
-
-	// Eval repartitions ( int % )
-	pnobody := float64(strings.Count(w.discussion, "_")) / float64(w.samples) * 100.0
-	pclient := float64(strings.Count(w.discussion, "c")) / float64(w.samples) * 100.0
-	pagent := float64(strings.Count(w.discussion, "a")) / float64(w.samples) * 100.0
-	pboth := float64(strings.Count(w.discussion, "#")) / float64(w.samples) * 100.0
-
-	// Update score according to repartions stats
-
-	// Si il y a eu beaucoup de collisions
-	if pboth > 20 {
-		a.raiseScore(int(30*pboth), "many collisions during discussion")
-	}
-
-	// Si l'agent à très peu parlé
-	if pagent < 10 {
-		a.raiseScore(1000, "agent didn't speak very much")
-	}
-
-	ratio := float64(a.score) / float64(w.samples)
-
-	if a.debug {
-		fmt.Printf("\nDISCUSSION: %s", w.discussion)
-	}
-	fmt.Printf("\nREPARTITION: silent=%.2f%%  client=%.2f%%  agent=%.2f%%  both=%.2f%%", pnobody, pclient, pagent, pboth)
-	fmt.Printf("\nRESULT     : samples=%d  score=%d  => [ RATIO = %.2f ]", w.samples, a.score, ratio)
-	fmt.Printf("\n>>\n")
+	a := Analyser{debug: true}
+	return a.run(schan)
 }
 
 func main() {
 
-	a := Analyser{debug: false}
-
-	//a.run(NewWaveform("samples/data_ok.json"))
-	//a.run(NewWaveform("samples/data_muted_agent_long.json"))
+	//analyseFile("samples/data_ok.json")
+	//analyseFile("samples/data_muted_agent_long.json")
 
 	files, _ := filepath.Glob("samples/*.json")
 	for _, file := range files {
-		a.run(NewWaveform(file))
+		analyseFile(file)
 	}
 }
